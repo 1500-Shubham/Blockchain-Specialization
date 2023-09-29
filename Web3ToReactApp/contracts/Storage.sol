d9b3ae1fc5d8115ab02347ab673e150ae0f34cfa
ShubhamVM <contactshubham1511@gmail.com>
2023-09-29 13:57:17 +0100

    firstDeployGanacheDone

---
+++ Web3ToReactApp/contracts/Storage.sol
@@ -0,0 +1,16 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+contract SimpleStorage {
+    uint256 public storedData;
+
+    // Function to set the storedData value
+    function set(uint256 _value) public {
+        storedData = _value;
+    }
+
+    // Function to get the storedData value
+    function get() public view returns (uint256) {
+        return storedData;
+    }
+}
